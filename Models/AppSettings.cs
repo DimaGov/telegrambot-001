9819c1c84a77bbecf06ad9ccbad6b07661c99633
Dima Govorukha <dimagov390@gmail.com>
2019-04-29 09:02:18 +0300

    Initial version

---
+++ Models/AppSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TelegramBotApp.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; set; }  = "https://telegrambotapp.azurewebsites.net:443/{0}";
+
+        public static string Name { get; set; } = "testBot";
+        
+        public static string Key { get; set; }  = "697473126:AAHq2K_ugx5T_crqoZE7N-cTC1USZHH4adk";
+
+    }
+}
